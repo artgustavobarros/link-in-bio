8bf050ae9edda6489eb052195014db9176a877ce
Arthur Barros <135854588+th3gust@users.noreply.github.com>
2024-07-16 08:22:33 -0300

    fixxing prettier error

--- src/components/tag/prefix.tsx
+++ src/components/tag/prefix.tsx
@@ -10,7 +10,8 @@ interface PrefixProps {
 export function Prefix({ children, url, title }: PrefixProps) {
   const childrenWithProps = cloneElement(children, { url })
   return (
-    <div className="lg:w-128 flex w-80 justify-center rounded-3xl bg-white p-5 shadow-md duration-300 hover:scale-110 hover:shadow-lg hover:ease-in-out">
+    // eslint-disable-next-line prettier/prettier
+    <div className="flex w-80 justify-center rounded-3xl bg-white p-5 shadow-md duration-300 hover:scale-110 hover:shadow-lg hover:ease-in-out lg:w-128">
       <Link href={url} target="_blank" className="flex-1">
         {title}
       </Link>
