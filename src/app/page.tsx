5a5da9c17cdcc6279cc4e2eb3dc17f9263a3fd86
Arthur Barros <135854588+th3gust@users.noreply.github.com>
2024-07-16 08:37:33 -0300

    fixin resposivite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import * as Tag from '../components/tag'
+import data from '@/links-to-share.json'
 
 export default function Home() {
   return (
-    <div className="space-y-5">
-      <section className="relative flex flex-col items-center justify-center gap-4 pt-16">
+    <div className="relative space-y-5">
+      <section className="flex flex-col items-center justify-center gap-4 pt-16">
         <Image
           src="/brand.jpg"
           alt=""
@@ -18,43 +19,20 @@ export default function Home() {
         <span className="text-md font-normal">
           Links e cupons disponísveis ♥
         </span>
-        <div className="fixed right-10 top-10 flex h-8 w-8 items-center justify-center rounded-full bg-white shadow-md duration-300 hover:scale-110 hover:shadow-lg hover:ease-in-out">
+        <div className="absolute right-5 top-5 flex h-8 w-8 items-center justify-center rounded-full bg-white shadow-md duration-300 hover:scale-110 hover:shadow-lg hover:ease-in-out">
           <Tag.DialogTag url="http://localhost:3000" />
         </div>
       </section>
       <section className="space-y-4 text-center text-sm font-light">
-        <Tag.Root>
-          <Tag.Prefix
-            url="https://www.youtube.com/@OxenteSoltaosCachos?si=eNxR__qL5jQ8EUgz"
-            title="youtube.com"
-          >
-            <Tag.DialogTag />
-          </Tag.Prefix>
-        </Tag.Root>
-        <Tag.Root>
-          <Tag.Prefix
-            url="https://www.legboxbrasil.com.br/pages/LEGOXENTESOLTAOSCACHOS?utm_source=leglover&utm_medium=link&utm_campaign=LEGOXENTESOLTAOSCACHOS"
-            title="LEGOXENTESOLTAOSCACHOS – Legbox"
-          >
-            <Tag.DialogTag />
-          </Tag.Prefix>
-        </Tag.Root>
-        <Tag.Root>
-          <Tag.Prefix
-            url="https://www.amazon.com.br/shop/oxentesoltaoscachos?ref=ac_inf_tb_vh"
-            title="Loja Amazon"
-          >
-            <Tag.DialogTag />
-          </Tag.Prefix>
-        </Tag.Root>
-        <Tag.Root>
-          <Tag.Prefix
-            url="https://www.embelleze.com/?gad_source=1&gclid=CjwKCAjwoPOwBhAeEiwAJuXRh0FEJxTqqZ1Ri2zfLM7uKI4TSMmd9hX9F62gBNov4FdTXr1LYPKWMxoC270QAvD_BwE"
-            title="Cupom: oxentesoltaoscachos / Embelleze"
-          >
-            <Tag.DialogTag />
-          </Tag.Prefix>
-        </Tag.Root>
+        {data.map((link) => {
+          return (
+            <Tag.Root key={link.title}>
+              <Tag.Prefix url={link.url} title={link.title}>
+                <Tag.DialogTag />
+              </Tag.Prefix>
+            </Tag.Root>
+          )
+        })}
       </section>
     </div>
   )
--- src/components/tag/prefix.tsx
+++ src/components/tag/prefix.tsx
@@ -11,7 +11,7 @@ export function Prefix({ children, url, title }: PrefixProps) {
   const childrenWithProps = cloneElement(children, { url })
   return (
     // eslint-disable-next-line prettier/prettier
-    <div className="flex w-80 justify-center rounded-3xl bg-white p-5 shadow-md duration-300 hover:scale-110 hover:shadow-lg hover:ease-in-out lg:w-128">
+    <div className="flex w-80 justify-center items-center rounded-3xl bg-white p-5 shadow-md duration-300 hover:scale-110 hover:shadow-lg hover:ease-in-out lg:w-[578px] lg:h-[78px] lg:font-semibold lg:text-base">
       <Link href={url} target="_blank" className="flex-1">
         {title}
       </Link>
