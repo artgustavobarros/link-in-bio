672cf8829dc6ad14a632746b3e111ca7780c3665
Arthur Barros <135854588+th3gust@users.noreply.github.com>
2024-09-10 16:42:18 -0300

    colors fix 0.1

--- src/components/tag/prefix.tsx
+++ src/components/tag/prefix.tsx
@@ -10,12 +10,11 @@ interface PrefixProps {
 export function Prefix({ children, url, title }: PrefixProps) {
   const childrenWithProps = cloneElement(children, { url })
   return (
-    // eslint-disable-next-line prettier/prettier
-    <div className='flex'>
+    <div className="flex">
       <Link
         href={url}
         target="_blank"
-        className="flex h-20 w-80 items-center justify-center rounded-full bg-white p-5 shadow-md duration-300 hover:scale-110 hover:shadow-lg hover:ease-in-out lg:h-[78px] lg:w-[578px] lg:text-base lg:font-semibold dark:bg-white"
+        className="flex h-20 w-80 items-center justify-center rounded-full bg-white p-5 shadow-md duration-300 hover:scale-110 hover:shadow-lg hover:ease-in-out dark:bg-white lg:h-[78px] lg:w-[578px] lg:text-base lg:font-semibold"
       >
         <span className="w-52 lg:w-full">{title}</span>
       </Link>
